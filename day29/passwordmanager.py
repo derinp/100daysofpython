bfa29020479cc6b501f9a4b442fe4a1af101bfcb
derinp <76889520+derinp@users.noreply.github.com>
2023-10-23 17:31:58 -0400

    Update passwordmanager.py
    Updated:
    added error handling for first time use.
    added error handling if a key is not found.
    changed from a txt format to a json file format for saved data.
    
    Added:
    Search functionality to retrieve a password previously added to the password.json file.

--- day29/passwordmanager.py
+++ day29/passwordmanager.py
@@ -3,6 +3,30 @@ from tkinter import messagebox
 import random
 import string
 import pyperclip
+import json
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("passwords.json") as file:
+            data = json.load(file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="error", message="No data file found")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            pyperclip.copy(password)
+            messagebox.showinfo(title=website, message=f"Username: {email}\n"
+                                                       f"Password: {password}\n"
+                                                       f"The password has been copied to your clipboard")
+
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website}")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -13,28 +37,38 @@ def generate_password():
     password_entry.insert(0, string=random_pass)
     pyperclip.copy(random_pass)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def get_entry_info():
-    # store text in the entry fields to a varialble.
+    # store text in the entry fields to a variable.
     website_entry_text = website_entry.get()
     user_name_text = username_entry.get()
     password_entry_text = password_entry.get()
+    new_data = {
+        website_entry_text: {
+            "email": user_name_text,
+            "password": password_entry_text
+        }
+    }
 
     if len(website_entry_text) == 0 or len(user_name_text) == 0 or len(password_entry_text) == 0:
         messagebox.showinfo(title="Empty Fields", message="There are some empty fields, unable to save.")
     else:
-        is_ok = messagebox.askokcancel(title=website_entry_text, message=f"Confirm.\n"
-                                                                         f"Website: {website_entry_text}\n"
-                                                                         f"Username: {user_name_text}\n"
-                                                                         f"Password: {password_entry_text}"
-                                                                         f"is this ok to save?")
-        if is_ok:
-            with open("passwords.txt", "a") as file:
-                file.write(f"{website_entry_text} | {user_name_text} | {password_entry_text}\n")
+        try:
+            with open("passwords.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("passwords.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
 
-            website_entry.delete(0, "end")
-            username_entry.delete(0, "end")
-            password_entry.delete(0, "end")
+            with open("passwords.json", "w") as file:
+                json.dump(data, file, indent=4)
+
+        website_entry.delete(0, "end")
+        username_entry.delete(0, "end")
+        password_entry.delete(0, "end")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,9 +85,12 @@ canvas.grid(column=0, row=0, columnspan=2)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=36)
+website_entry = Entry(width=24)
 website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="w")
+
+search_button = Button(text="Search", width=8, command=find_password)
+search_button.grid(column=1, row=1, sticky="e")
 
 # username/email row
 username_label = Label(text="Email/Username:")
@@ -78,4 +115,3 @@ add_button = Button(text="Add password", width=30, command=get_entry_info)
 add_button.grid(column=1, row=4, columnspan=2, sticky="w")
 
 window.mainloop()
-
